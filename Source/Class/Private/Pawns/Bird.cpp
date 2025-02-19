047cd4b73b0c467e161c5b700cf6710a40fd75a6
bsack23 <bsack23@gmail.com>
2025-02-19 08:58:59 -0500

    added capsule to Bird class

--- Source/Class/Private/Pawns/Bird.cpp
+++ Source/Class/Private/Pawns/Bird.cpp
@@ -1,34 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Pawns/Bird.h"
+// #include "Components/CapsuleComponent.h"
 
-// Sets default values
-ABird::ABird()
-{
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+// Sets default values 
+// This is the class constructor! runs once when we make a new instance of the class
+ABird::ABird() {
+  // Set this pawn to call Tick() every frame.  You can turn this off to improve
+  // performance if you don't need it.
+  PrimaryActorTick.bCanEverTick = true;
+  // this adds the Capsule 
+  Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+  // and sets it as the root component
+  SetRootComponent(Capsule);
 }
 
 // Called when the game starts or when spawned
-void ABird::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
+void ABird::BeginPlay() { Super::BeginPlay(); }
 
 // Called every frame
-void ABird::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
+void ABird::Tick(float DeltaTime) { Super::Tick(DeltaTime); }
 
 // Called to bind functionality to input
-void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+void ABird::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent) {
+  Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
-
--- Source/Class/Public/Pawns/Bird.h
+++ Source/Class/Public/Pawns/Bird.h
@@ -1,29 +1,33 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+// clang-format off
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+// this is only here temporarily until we forward declare!
+#include "Components/CapsuleComponent.h"
 #include "Bird.generated.h"
 
 UCLASS()
-class CLASS_API ABird : public APawn
-{
-	GENERATED_BODY()
+class CLASS_API ABird : public APawn {
+  GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
-	ABird();
+  // Sets default values for this pawn's properties
+  ABird();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+  // Called when the game starts or when spawned
+  virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+  // Called every frame
+  virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+  // Called to bind functionality to input
+  virtual void SetupPlayerInputComponent(
+      class UInputComponent *PlayerInputComponent) override;
+private:
+  UPROPERTY(VisibleAnywhere)
+  UCapsuleComponent *Capsule; 
 };

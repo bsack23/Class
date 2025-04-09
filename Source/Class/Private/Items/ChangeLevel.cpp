a621fc474021a19f462c5d322ba423229c313957
bsack23 <bsack23@gmail.com>
2025-04-09 16:20:58 -0400

    added ChangeLevel class

---
+++ Source/Class/Private/Items/ChangeLevel.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Items/ChangeLevel.h"
+#include "Characters/MyCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+// check it out
+// https://dev.epicgames.com/documentation/en-us/unreal-engine/API/Runtime/Engine/Kismet/UGameplayStatics/OpenLevel
+
+void AChangeLevel::OnOverlapBegin(class UPrimitiveComponent *OverlappedComp,
+                                  class AActor *OtherActor,
+                                  class UPrimitiveComponent *OtherComp,
+                                  int32 OtherBodyIndex, bool bFromSweep,
+                                  const FHitResult &SweepResult) {
+  // use Super to call version of this func from parent class (Item)
+  Super::OnOverlapBegin(OverlappedComp, OtherActor, OtherComp, OtherBodyIndex,
+                        bFromSweep, SweepResult);
+  // cast from MyCharacter class so we can move the character
+  AMyCharacter *MyCharacter = Cast<AMyCharacter>(OtherActor);
+  // check for valid Character
+  if (MyCharacter) {
+    UWorld *TheWorld = GetWorld();
+
+    FString CurrentLevel = TheWorld->GetMapName();
+
+    if (CurrentLevel == CurrLevel) {
+
+      UGameplayStatics::OpenLevel(TheWorld, ToLevel);
+    } else {
+      UGameplayStatics::OpenLevel(TheWorld, CurrLevel);
+    }
+  }
+}
---
+++ Source/Class/Public/Items/ChangeLevel.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+// clang-format off
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+
+#include "ChangeLevel.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class CLASS_API AChangeLevel : public AItem {
+  GENERATED_BODY()
+protected:
+  // redeclare functions as overrides from the parent versions in Item.h
+  // can't use UFUNCTION here because they're already declared in Item.h
+  // UFUNCTION()
+  virtual void OnOverlapBegin(class UPrimitiveComponent *OverlappedComp,
+                              class AActor *OtherActor,
+                              class UPrimitiveComponent *OtherComp,
+                              int32 OtherBodyIndex, bool bFromSweep,
+                              const FHitResult &SweepResult) override;
+// properties so we can assign different levels in BP child class
+UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Level Parameters")
+FName CurrLevel;
+
+UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Level Parameters")
+FName ToLevel;
+
+};
